9dea2522d4b2badec5bf03caef646b10e98ea001
Toochidennis <dennistoochukwu@gmail.com>
2023-11-06 10:29:45 +0100

    configs

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,4 +8,8 @@ module com.toochi.resultmanagement {
 
     opens com.toochi.resultmanagement to javafx.fxml;
     exports com.toochi.resultmanagement;
+    exports com.toochi.resultmanagement.controllers;
+    opens com.toochi.resultmanagement.controllers to javafx.fxml;
+    exports com.toochi.resultmanagement.launcher;
+    opens com.toochi.resultmanagement.launcher to javafx.fxml;
 }
