4f8e7583cb803c96aa528b06347c62791251f79c
Toochidennis <dennistoochukwu@gmail.com>
2023-11-06 10:31:43 +0100

    created Config.kt for db setup

---
+++ src/main/java/com/toochi/resultmanagement/backend/Config.kt
@@ -0,0 +1,21 @@
+package com.toochi.resultmanagement.backend
+
+import java.util.Properties
+
+class Config {
+    private val properties = Properties()
+
+    init {
+        javaClass.classLoader.getResourceAsStream("com/toochi/resultmanagement/config/config.properties").use {
+            properties.load(it)
+        }
+    }
+
+    val dbUrl: String by properties
+    val dbUser: String by properties
+    val dbPassword: String by properties
+    val dbDatabase: String by properties
+    val dbSettingsTable: String by properties
+    val dbStudentsTable: String by properties
+    val dbResultsTable: String by properties
+}
