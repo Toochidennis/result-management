059b1e242419ddbf0f3cc847a43aa1efd5a1a247
Toochidennis <dennistoochukwu@gmail.com>
2023-11-06 10:44:09 +0100

    Added Launcher.java

---
+++ src/main/java/com/toochi/resultmanagement/launcher/Launcher.java
@@ -0,0 +1,35 @@
+package com.toochi.resultmanagement.launcher;
+
+import com.toochi.resultmanagement.backend.ConnectionProvider;
+import com.toochi.resultmanagement.backend.Config;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Scene;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Launcher extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Launcher.class.getResource("/com/toochi/resultmanagement/dashboard/dashboard_view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Hello!");
+
+        Screen screen = Screen.getPrimary();
+        Rectangle2D bounds = screen.getVisualBounds();
+        stage.setX(bounds.getMinX());
+        stage.setY(bounds.getMinY());
+        stage.setWidth(bounds.getWidth());
+        stage.setHeight(bounds.getHeight());
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        new ConnectionProvider(new Config()).getConnection();
+        //launch();
+    }
+}
