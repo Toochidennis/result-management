27ac9b553fb22f92ca7258d1272a5208e0904e64
Toochidennis <dennistoochukwu@gmail.com>
2023-11-06 10:36:20 +0100

    Added Util

--- src/main/java/com/toochi/resultmanagement/Util.kt
+++
@@ -1,3 +0,0 @@
-package com.toochi.resultmanagement
-
-data class Util(val message:String)
---
+++ src/main/java/com/toochi/resultmanagement/utils/Util.kt
@@ -0,0 +1,24 @@
+package com.toochi.resultmanagement.utils
+
+import javafx.fxml.FXMLLoader
+import javafx.scene.Node
+import javafx.scene.layout.AnchorPane
+import javafx.scene.layout.BorderPane
+import java.lang.Exception
+
+object Util {
+
+    @JvmStatic
+    fun loadFXMLScene(root: BorderPane, fxmlFileName: String) {
+        try {
+            val loader = FXMLLoader(javaClass.getResource("/com/toochi/resultmanagement/$fxmlFileName"))
+            val layout = loader.load<AnchorPane>()
+            root.center = layout
+
+            println("loaded $fxmlFileName")
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+}
