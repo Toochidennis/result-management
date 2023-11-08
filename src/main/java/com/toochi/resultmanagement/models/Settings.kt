422d4b7b722080c8cd992b157b3ed3dc1fb82765
Toochidennis <dennistoochukwu@gmail.com>
2023-11-08 08:42:38 +0100

    added functions for making queries

--- src/main/java/com/toochi/resultmanagement/models/Settings.kt
+++ src/main/java/com/toochi/resultmanagement/models/Settings.kt
@@ -1,4 +1,37 @@
 package com.toochi.resultmanagement.models
 
-class Settings {
+import javafx.beans.property.SimpleDoubleProperty
+import javafx.beans.property.SimpleStringProperty
+
+class Settings(
+    val serialNumber: Int,
+    val courseId: Int,
+    val courseName: String,
+    val courseCode: String,
+    val courseUnits: Int,
+    val programme: String = "",
+    val semester: String = ""
+) {
+    val gradeLaterProperty = SimpleStringProperty()
+    val gradePointProperty = SimpleDoubleProperty()
+    val totalProperty = SimpleDoubleProperty()
+
+    init {
+        gradeLaterProperty.addListener { _, _, _ ->
+            calculateGradePointAndTotal()
+        }
+    }
+
+    private fun calculateGradePointAndTotal() {
+        when (gradeLaterProperty.value) {
+            "A" -> gradePointProperty.value = 5.0
+            "B" -> gradePointProperty.value = 4.0
+            "C" -> gradePointProperty.value = 3.0
+            else -> gradePointProperty.value = 0.0
+        }
+
+        totalProperty.set(gradePointProperty.get() * courseUnits)
+    }
+
+
 }
