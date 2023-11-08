112ca457b13479656b3b14e3e6e735ed54a52144
Toochidennis <dennistoochukwu@gmail.com>
2023-11-09 00:46:56 +0100

    Update StudentController.kt

--- src/main/java/com/toochi/resultmanagement/controllers/StudentController.kt
+++ src/main/java/com/toochi/resultmanagement/controllers/StudentController.kt
@@ -1,54 +1,96 @@
 package com.toochi.resultmanagement.controllers
 
+import com.toochi.resultmanagement.backend.QueryExecutor.executeInsertQuery
+import com.toochi.resultmanagement.utils.Util.generateProgrammes
+import com.toochi.resultmanagement.utils.Util.generateSessions
 import javafx.fxml.FXML
 import javafx.scene.control.ComboBox
 import javafx.scene.control.DatePicker
 import javafx.scene.control.TextField
-import javafx.scene.input.MouseEvent
 
 class StudentController {
 
+
     @FXML
-    private lateinit var addressTxt: TextField
+    private lateinit var firstNameTextField: TextField
+
+    @FXML
+    private lateinit var middleNameTextField: TextField
+
+    @FXML
+    private lateinit var lastNameTextField: TextField
+
+    @FXML
+    private lateinit var matricNumberTextField: TextField
+
+    @FXML
+    private lateinit var phoneNumberTextField: TextField
+
+    @FXML
+    private lateinit var disciplineTextField: TextField
+
+    @FXML
+    private lateinit var sessionComboBox: ComboBox<String>
+
+    @FXML
+    private lateinit var programmeComboBox: ComboBox<String>
 
     @FXML
     private lateinit var dateOfBirthPicker: DatePicker
 
     @FXML
-    private lateinit var disciplineTxt: TextField
+    private lateinit var emailTextField: TextField
 
     @FXML
-    private lateinit var emailTxt: TextField
+    private lateinit var addressTextField: TextField
 
     @FXML
-    private lateinit var firstNameTxt: TextField
-
-    @FXML
-    private lateinit var lastNameTxt: TextField
-
-    @FXML
-    private lateinit var matricNumberTxt: TextField
-
-    @FXML
-    private lateinit var middleNameTxt: TextField
-
-    @FXML
-    private lateinit var phoneNumberTxt: TextField
-
-    @FXML
-    private lateinit var semesterCombo: ComboBox<Any>
-
-    @FXML
-    private lateinit var sessionCombo: ComboBox<Any>
-
-    @FXML
-    fun semesterClick(event: MouseEvent) {
-
+    fun initialize() {
+        createProgrammes()
+        createSessions()
     }
 
     @FXML
-    fun sessionClick(event: MouseEvent) {
+    fun submitBtn(){
+        getTextFromViews()
+    }
 
+    private fun createProgrammes() {
+        programmeComboBox.items = generateProgrammes()
+    }
+
+    private fun createSessions() {
+        sessionComboBox.items = generateSessions()
+    }
+
+    private fun getTextFromViews() {
+        val firstName = firstNameTextField.text
+        val middleName = middleNameTextField.text
+        val lastName = lastNameTextField.text
+        val matricNumber = matricNumberTextField.text
+        val phoneNumber = phoneNumberTextField.text
+        val discipline = disciplineTextField.text
+        val session = sessionComboBox.value
+        val programme = programmeComboBox.value
+        val dateOfBirth = dateOfBirthPicker.value
+        val email = emailTextField.text
+        val address = addressTextField.text
+
+        val values = HashMap<String, Any>().apply {
+            put("first_name", firstName)
+            put("middle_name", middleName)
+            put("last_name", lastName)
+            put("matric_number", matricNumber)
+            put("phone_number", phoneNumber)
+            put("discipline", discipline)
+            put("session", session)
+            put("programme", programme)
+            put("date_of_birth", "$dateOfBirth")
+            put("email", email)
+            put("address", address)
+        }
+
+        executeInsertQuery("students", values)
     }
 
 }
