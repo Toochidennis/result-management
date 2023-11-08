053daf54c7156397364f31939fe317037e552cc3
Toochidennis <dennistoochukwu@gmail.com>
2023-11-09 00:45:47 +0100

    Update Student.kt

--- src/main/java/com/toochi/resultmanagement/models/Student.kt
+++ src/main/java/com/toochi/resultmanagement/models/Student.kt
@@ -1,4 +1,16 @@
 package com.toochi.resultmanagement.models
 
-class Student {
-}
+data class Student(
+    val studentId: String,
+    val firstName: String,
+    val middleName: String,
+    val lastName: String,
+    val matricNumber:String,
+    val phoneNumber: String,
+    val discipline: String,
+    val session: String,
+    val programme: String,
+    val dateOfBirth: String,
+    val email: String,
+    val address: String
+)
