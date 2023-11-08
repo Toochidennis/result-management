40f54b9586c6b92157e62ad477e6b74d01905939
Toochidennis <dennistoochukwu@gmail.com>
2023-11-08 08:41:44 +0100

    added functions for making queries

--- src/main/java/com/toochi/resultmanagement/backend/QueryExecutor.kt
+++ src/main/java/com/toochi/resultmanagement/backend/QueryExecutor.kt
@@ -1,29 +1,106 @@
 package com.toochi.resultmanagement.backend
 
+import java.sql.Connection
+import java.sql.PreparedStatement
 import java.sql.ResultSet
 
-class QueryExecutor() {
+object QueryExecutor {
 
-    private var connectionProvider: ConnectionProvider? = null
+    private var connection: Connection? = null
 
     init {
-        connectionProvider = ConnectionProvider(Config())
+        connection = ConnectionProvider(Config()).getConnection()
     }
 
-    fun executeQuery(sql: String): ResultSet? {
+    @JvmStatic
+    fun executeInsertQuery(tableName: String, values: HashMap<String, Any>):Int {
+        val sql = generateInsertQuery(tableName, values)
+        var affectedRows = 0
+
+        connection?.prepareStatement(sql)?.use {
+            setParameters(it, values.values.toMutableList())
+            affectedRows = it.executeUpdate()
+        }
+
+        return affectedRows
+    }
+
+    @JvmStatic
+    fun executeSelectQueryWithConditions(
+        tableName: String,
+        conditions: HashMap<String, Any>
+    ): ResultSet? {
+        val sql = generateSelectQueryWithConditionsQuery(tableName, conditions)
         var resultSet: ResultSet? = null
-        connectionProvider?.getConnection()?.run {
-            try {
-                val preparedStatement =
-                    prepareStatement(sql, ResultSet.CONCUR_READ_ONLY, ResultSet.TYPE_SCROLL_INSENSITIVE)
-                resultSet = preparedStatement.executeQuery()
-            } catch (e: Exception) {
-                e.printStackTrace()
-            } finally {
-                close()
-            }
+
+        connection?.prepareStatement(sql, ResultSet.CONCUR_READ_ONLY)?.use {
+            setParameters(it, conditions.values.toMutableList())
+            resultSet = it.executeQuery()
         }
 
         return resultSet
     }
+
+    @JvmStatic
+    fun executeSelectAllQuery(tableName: String): ResultSet? {
+        var resultSet: ResultSet? = null
+        val sql = "SELECT * FROM $tableName"
+
+        connection?.prepareStatement(sql, ResultSet.CONCUR_READ_ONLY)?.use {
+            resultSet = it.executeQuery()
+        }
+
+        return resultSet
+    }
+
+    @JvmStatic
+    fun executeUpdateQuery(
+        tableName: String,
+        values: HashMap<String, Any>,
+        conditions: HashMap<String, Any>
+    ): Int {
+        val sql = generateUpdateQuery(tableName, values, conditions)
+        var affectedRows = 0
+        val allParameters = values.values.toMutableList() + conditions.values.toMutableList()
+
+        connection?.prepareStatement(sql, ResultSet.CONCUR_UPDATABLE)?.use {
+            setParameters(it, allParameters.toMutableList())
+            affectedRows = it.executeUpdate()
+        }
+
+        return affectedRows
+    }
+
+
+    private fun generateInsertQuery(tableName: String, values: HashMap<String, Any>): String {
+        val columns = values.keys.joinToString(", ") { it }
+        val placeHolders = values.keys.joinToString(", ") { "?" }
+        return "INSERT INTO $tableName ($columns) VALUES ($placeHolders);"
+    }
+
+    private fun generateSelectQueryWithConditionsQuery(
+        tableName: String,
+        conditions: HashMap<String, Any>
+    ): String {
+        val columns = conditions.keys.joinToString { ", " }
+        val whereClause = conditions.keys.joinToString(" AND ") { "$it = ?" }
+        return "SELECT $columns FROM $tableName WHERE $whereClause;"
+    }
+
+    private fun generateUpdateQuery(
+        tableName: String,
+        values: HashMap<String, Any>,
+        conditions: HashMap<String, Any>
+    ): String {
+        val setClause = values.keys.joinToString(", ") { "$it = ?" }
+        val whereClause = conditions.keys.joinToString(", ") { "$it = ?" }
+        return "UPDATE $tableName SET $setClause WHERE $whereClause"
+    }
+
+    private fun setParameters(preparedStatement: PreparedStatement, parameters: MutableList<Any>) {
+        parameters.forEachIndexed { index, parameter ->
+            preparedStatement.setObject(index + 1, parameter)
+        }
+    }
+
 }
