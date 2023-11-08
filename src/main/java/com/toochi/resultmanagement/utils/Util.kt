aad708c7bd36a32b5af285053b0d55615b78ae10
Toochidennis <dennistoochukwu@gmail.com>
2023-11-08 08:44:26 +0100

    added utils function

--- src/main/java/com/toochi/resultmanagement/utils/Util.kt
+++ src/main/java/com/toochi/resultmanagement/utils/Util.kt
@@ -1,22 +1,40 @@
 package com.toochi.resultmanagement.utils
 
+import javafx.collections.FXCollections
 import javafx.fxml.FXMLLoader
-import javafx.scene.Node
 import javafx.scene.layout.AnchorPane
 import javafx.scene.layout.BorderPane
-import java.lang.Exception
+import java.time.Year
 
 object Util {
 
     @JvmStatic
     fun loadFXMLScene(root: BorderPane, fxmlFileName: String) {
         try {
-            val loader = FXMLLoader(javaClass.getResource("/com/toochi/resultmanagement/$fxmlFileName"))
+            val loader =
+                FXMLLoader(javaClass.getResource("/com/toochi/resultmanagement/$fxmlFileName"))
             val layout = loader.load<AnchorPane>()
             root.center = layout
         } catch (e: Exception) {
             e.printStackTrace()
         }
     }
+
+    @JvmStatic
+    fun generateSessions(): List<String> {
+        return (2005..2030)
+            .map(Year::of)
+            .map(Year::toString)
+            .toCollection(ArrayList())
+    }
+
+    @JvmStatic
+    fun generateSemesters() = FXCollections.observableArrayList(
+        "First semester",
+        "Second semester"
+    )
+
+    @JvmStatic
+    fun generateProgrammes()= FXCollections.observableArrayList("PGD", "MBA","MSC")
 
 }
