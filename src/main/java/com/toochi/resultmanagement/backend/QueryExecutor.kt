34cd93d34e2e325e75769bfe48214d6e4350f904
Toochidennis <dennistoochukwu@gmail.com>
2023-11-14 17:06:28 +0100

    Update QueryExecutor.kt

--- src/main/java/com/toochi/resultmanagement/backend/QueryExecutor.kt
+++ src/main/java/com/toochi/resultmanagement/backend/QueryExecutor.kt
@@ -5,6 +5,136 @@ import java.nio.file.Path
 import java.sql.PreparedStatement
 import java.sql.ResultSet
 
+
+/**
+ * Package Documentation: `com.toochi.resultmanagement.backend`
+ *
+ * Overview
+ * ------------
+ * The `com.toochi.resultmanagement.backend` package provides functionality for executing various SQL queries
+ * and interacting with the database. It includes methods for inserting records, updating records, and executing
+ * select queries with conditions. Additionally, there are methods for executing specific queries related to
+ * retrieving student information with associated results.
+ *
+ * Classes and Methods
+ * --------------------------
+ *
+ * ### `QueryExecutor`
+ *
+ * This class serves as a utility for executing SQL queries and interacting with the database.
+ *
+ * #### Methods:
+ *
+ * 1. **`executeInsertQuery`**
+ *    - Purpose: Executes an insert query on the specified table with the provided values.
+ *    - Parameters:
+ *      - `tableName` (String): The name of the table to insert into.
+ *      - `values` (HashMap<String, Any>): A map containing column names and their corresponding values to be inserted.
+ *    - Returns: The number of affected rows.
+ *
+ * 2. **`executeSelectWithConditionsQuery2`**
+ *    - Purpose: Executes a select query with conditions on the specified table.
+ *    - Parameters:
+ *      - `tableName` (String): The name of the table to query.
+ *      - `conditions` (HashMap<String, Any>): A map containing column names and their corresponding values for conditions.
+ *    - Returns: A `ResultSet` containing the query results.
+ *
+ * 3. **`executeSelectAllQuery`**
+ *    - Purpose: Executes a select all query on the specified table.
+ *    - Parameters:
+ *      - `tableName` (String): The name of the table to query.
+ *    - Returns: A `ResultSet` containing the query results.
+ *
+ * 4. **`executeUpdateQuery`**
+ *    - Purpose: Executes an update query on the specified table with the provided values and conditions.
+ *    - Parameters:
+ *      - `tableName` (String): The name of the table to update.
+ *      - `values` (HashMap<String, Any>): A map containing column names and their corresponding values to be updated.
+ *      - `conditions` (HashMap<String, Any>): A map containing column names and their corresponding values for conditions.
+ *    - Returns: The number of affected rows.
+ *
+ * 5. **`executeAllTablesQuery`**
+ *    - Purpose: Executes a complex query to retrieve data from multiple tables related to a specific student.
+ *    - Parameters:
+ *      - `studentId` (Int): The ID of the student for whom to retrieve data.
+ *    - Returns: A `ResultSet` containing the query results.
+ *
+ * 6. **`executeStudentsWithResultQuery`**
+ *    - Purpose: Executes a query to retrieve distinct student records with associated results.
+ *    - Parameters:
+ *      - `tableName` (String): The name of the table representing students.
+ *    - Returns: A `ResultSet` containing the query results.
+ *
+ * 7. **`setParameters`**
+ *    - Purpose: Sets parameters on a prepared statement based on the provided values.
+ *    - Parameters:
+ *      - `preparedStatement` (PreparedStatement): The prepared statement to set parameters on.
+ *      - `parameters` (MutableList<Any>): A list of parameters to be set on the prepared statement.
+ *
+ * Usage Examples
+ * -------------------
+ *
+ * #### Inserting Records
+ *
+ * ```kotlin
+ * val values = hashMapOf(
+ *     "column1" to value1,
+ *     "column2" to value2,
+ *     // Add additional columns and values as needed
+ * )
+ *
+ * val affectedRows = QueryExecutor.executeInsertQuery("tableName", values)
+ * ```
+ *
+ * #### Selecting Records with Conditions
+ *
+ * ```kotlin
+ * val conditions = hashMapOf(
+ *     "column1" to value1,
+ *     "column2" to value2,
+ *     // Add additional columns and values as needed
+ * )
+ *
+ * val resultSet = QueryExecutor.executeSelectWithConditionsQuery2("tableName", conditions)
+ * ```
+ *
+ * #### Updating Records
+ *
+ * ```kotlin
+ * val values = hashMapOf(
+ *     "column1" to newValue1,
+ *     "column2" to newValue2,
+ *     // Add additional columns and values as needed
+ * )
+ *
+ * val conditions = hashMapOf(
+ *     "conditionColumn" to conditionValue
+ * )
+ *
+ * val affectedRows = QueryExecutor.executeUpdateQuery("tableName", values, conditions)
+ * ```
+ *
+ * #### Retrieving Data from Multiple Tables
+ *
+ * ```kotlin
+ * val studentId = 123
+ * val resultSet = QueryExecutor.executeAllTablesQuery(studentId)
+ * ```
+ *
+ * #### Retrieving Distinct Students with Results
+ *
+ * ```kotlin
+ * val tableName = "students"
+ * val resultSet = QueryExecutor.executeStudentsWithResultQuery(tableName)
+ * ```
+ *
+ * Note
+ * --------
+ * - Ensure that the database connection is properly configured using the `Config` and `ConnectionProvider` classes.
+ * - The examples provided cover common use cases, and you can adapt them to suit the specific requirements of your application.
+ * - Use the appropriate methods based on the type of query you want to execute (insert, select, update) and whether conditions are involved.
+ */
+
 object QueryExecutor {
 
     private fun connection() = ConnectionProvider(Config()).getConnection()
@@ -16,31 +146,13 @@ object QueryExecutor {
         var affectedRows = 0
 
         connection()?.use { conn ->
-            conn.prepareStatement(sql).use {
+            conn.prepareStatement(sql, ResultSet.CONCUR_UPDATABLE).use {
                 setParameters(it, values.values.toMutableList())
                 affectedRows = it.executeUpdate()
             }
         }
 
         return affectedRows
-    }
-
-    @JvmStatic
-    fun executeSelectWithConditionsQuery(
-        tableName: String,
-        conditions: HashMap<String, Any>
-    ): ResultSet? {
-        val sql = generateSelectQueryWithConditionsQuery(tableName, conditions)
-
-        connection()?.use { conn ->
-            conn.prepareStatement(sql, ResultSet.CONCUR_READ_ONLY).use {
-                setParameters(it, conditions.values.toMutableList())
-                resultSet = it.executeQuery()
-
-            }
-        }
-
-        return resultSet
     }
 
     @JvmStatic
@@ -97,7 +209,7 @@ object QueryExecutor {
         val sql = generateAllTablesQuery()
 
         connection()?.use { conn ->
-            conn.prepareStatement(sql)?.use {
+            conn.prepareStatement(sql, ResultSet.CONCUR_READ_ONLY)?.use {
                 setParameters(it, mutableListOf(studentId))
                 resultSet = it.executeQuery()
             }
@@ -111,7 +223,7 @@ object QueryExecutor {
         val sql = generateStudentsWithResultQuery(tableName)
 
         connection()?.use { conn ->
-            conn.prepareStatement(sql)?.use {
+            conn.prepareStatement(sql, ResultSet.CONCUR_READ_ONLY)?.use {
                 resultSet = it.executeQuery()
             }
         }
@@ -119,19 +231,30 @@ object QueryExecutor {
         return resultSet
     }
 
+
+    @JvmStatic
+    fun searchStudentByLastDigits(lastDigits: String): ResultSet? {
+        val sql = generateSearchQuery()
+        val likePattern = "%/$lastDigits"
+
+        connection()?.use { conn ->
+            conn.prepareStatement(sql, ResultSet.CONCUR_READ_ONLY).use {
+                setParameters(it, mutableListOf(likePattern))
+                resultSet = it.executeQuery()
+            }
+        }
+
+        return resultSet
+    }
+
+    private fun generateSearchQuery(): String {
+        return "SELECT * FROM students WHERE registration_number LIKE ?"
+    }
+
     private fun generateInsertQuery(tableName: String, values: HashMap<String, Any>): String {
         val columns = values.keys.joinToString(", ") { it }
         val placeHolders = values.keys.joinToString(", ") { "?" }
         return "INSERT INTO $tableName ($columns) VALUES ($placeHolders)"
-    }
-
-    private fun generateSelectQueryWithConditionsQuery(
-        tableName: String,
-        conditions: HashMap<String, Any>
-    ): String {
-        val columns = conditions.keys.joinToString { ", " }
-        val whereClause = conditions.keys.joinToString(" AND ") { "$it = ?" }
-        return "SELECT $columns FROM $tableName WHERE $whereClause"
     }
 
     private fun generateSelectQueryById(
