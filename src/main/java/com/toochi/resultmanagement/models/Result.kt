2d8ed329bd0d79193cfdc8ed5219e3480ad58da0
Toochidennis <dennistoochukwu@gmail.com>
2023-11-22 21:29:32 +0100

    Updated Result.kt

---
+++ src/main/java/com/toochi/resultmanagement/models/Result.kt
@@ -0,0 +1,11 @@
+package com.toochi.resultmanagement.models
+
+data class Result(
+    val id: String,
+    val code: String,
+    val title: String,
+    val unit: String,
+    val grade: String,
+    val gradePoint: String,
+    val wgp: String
+)
